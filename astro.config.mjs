5a0da67d271bdd9f7987b0501f37af90b3401200
peerapat khiewchaoum <peerapat.kh2002@gmail.com>
2025-08-21 16:25:03 +0700

    Changes too astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+     site: 'https://peerapat.kh2002.github.io',
+  base: 'Project',
+});
